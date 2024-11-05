c3c3270b107c060dcab08572b8826efdf5780f5b
Elkin-M <165603522+Elkin-M@users.noreply.github.com>
2024-11-05 13:46:54 -0500

    Actualizacion de cambios

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,20 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const sidebar = document.getElementById('sidebar');
     const toggleButton = document.getElementById('toggleSidebar');
     
-    // Asegurarse de que el sidebar comience cerrado
     body.classList.remove('sidebar-active');
     sidebar.classList.remove('active');
     
-    // Manejar el toggle del sidebar
     toggleButton.addEventListener('click', function() {
         body.classList.toggle('sidebar-active');
         sidebar.classList.toggle('active');
         
-        // Opcional: Guardar el estado en localStorage para mantener la preferencia del usuario
         localStorage.setItem('sidebarOpen', body.classList.contains('sidebar-active'));
     });
     
-    // Opcional: Cerrar el sidebar en dispositivos móviles cuando se hace clic fuera
     document.addEventListener('click', function(event) {
         const isClickInsideSidebar = sidebar.contains(event.target);
         const isClickOnToggleButton = toggleButton.contains(event.target);
