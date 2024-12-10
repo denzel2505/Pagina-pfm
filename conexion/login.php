123ffefcf2cc58d5390482b1d95d0b443e379a42
Elkin Miranda <elkinmb3@gmail.com>
2024-12-10 09:08:07 -0500

    correcion de conexiones

--- conexion/login.php
+++ conexion/login.php
@@ -1,30 +1,25 @@
 <?php
-include 'db.php'; // Asegúrate de que db.php contiene la conexión a la base de datos
+include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'] ?? '';
     $contraseña = $_POST['contraseña'] ?? '';
 
-    // Usar sentencia preparada para evitar inyección SQL
+    
     $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
     
-    // Vincular el parámetro con bindParam()
     $stmt->bindParam(':email', $email, PDO::PARAM_STR);
 
-    // Ejecutar la consulta
     $stmt->execute();
     
-    // Obtener el resultado de la consulta
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($result) {
-        // Verificar la contraseña
         if (password_verify($contraseña, $result['password'])) {
             echo "Inicio de sesión exitoso";
 
-            // Redirigir a la página de inicio
             header('Location: ../inicio.html');
-            exit(); // Asegurarse de que el script se detiene después de redirigir
+            exit(); 
         } else {
             echo "Contraseña incorrecta";
         }
